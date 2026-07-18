4538bf4f2ca61e8b6208d6e6bf4f869c206901f2
deepakanandap-arch <deepakanandap@gmail.com>
2026-07-18 04:45:38 +0000

    Add JavaScript: make Follow button change text on click

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("My script is connecterd")
+let followButton = document.querySelector(".follow-button");
+
+followButton.addEventListener("click", function() {
+followButton.textContent = "following";
+}
+)
